c6150389d3320ff6ade39f180740194d6baf3578
dima137 <dvmalyshev@gmail.com>
2014-05-19 16:36:12 -0700

    normalization included in dima's selector

--- match_dima/tea_arxiv.py
+++ match_dima/tea_arxiv.py
@@ -5,6 +5,8 @@ import xml.etree.ElementTree as ET
 import cPickle as pickle
 import numpy as np
 
+POWER = 1
+
 def subsplit(words, string):
     res = []
     for word in words:
@@ -45,23 +47,29 @@ def abs2vector(url):
 def get_metric(wdict):
     res = {}
     for key, value in wdict.iteritems():
-        res[key] = 1. / value**2
+        res[key] = 1. / value**POWER
     return res
-        
-def prod(g, a, b):
+
+def norm(a):
+    if len(a) > 0:
+        return np.sqrt(np.sum(np.array(a.values())**2))
+    else:
+        return 1
+
+def prod(g, g_norm, a, b):
     res = 0.
     for key, value in b.iteritems():
         val = g.get(key, 0.) * a.get(key, 0.) * value
-        #if key in g and key in a:
-        #    print '%s\t\t %.5f' % (key, val)
         res += val
+    res *= (g_norm / (norm(a) * norm(b)))**0.5
     return res
 
-def suggest_people(arxiv_id, people_fn='kipackizens.pkl', thres=0.1):
+def suggest_people(arxiv_id, people_fn='kipackizens.pkl', thres=1.):
     pfile = open(people_fn)
     people_dict = pickle.load(pfile)
     all_word_dict = people_dict.pop('All words')
     g = get_metric(all_word_dict)
+    g_norm = norm(all_word_dict)
     
     url = 'http://export.arxiv.org/api/query?id_list='+arxiv_id+'&max_results=1'
     abs_dict = abs2vector(url)
@@ -71,12 +79,10 @@ def suggest_people(arxiv_id, people_fn='kipackizens.pkl', thres=0.1):
 
     npeople = 0
     for i, person in enumerate(people_dict.keys()):
-        #print person
-        rank = prod(g, people_dict[person]['keywords'], abs_dict)
+        rank = prod(g, g_norm, people_dict[person]['keywords'], abs_dict)
         ranks[i] = (person, rank)
         if rank > thres:
             npeople += 1
-        #print
     ranks = np.sort(ranks, order='rank')[::-1]
     
     return ranks[:npeople]
